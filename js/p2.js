1643ef27165e2ebea624a9deb0d9cd949d789273
JohnnyMSnake <l22550321@chihuahua2.tecnm.mx>
2024-11-14 21:33:03 -0600

    elaboracion de practica 2

---
+++ js/p2.js
@@ -0,0 +1,13 @@
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSumbit)
+
+function handleSumbit(event){
+    event.preventDefault()
+    console.log("funciona esta mierda xdxdxxdx")
+    let confirmacion = prompt("Los datos que ingreso son correctos? ingrese 'si' si lo estan ")
+    if(confirmacion === "si"){
+        const form = new FormData(this)
+        alert("hola " + form.get('nombre') + " tu edad es " + form.get('edad') + " y tu pais de residencia es " + form.get('pais') + " UwU")
+    }
+}
