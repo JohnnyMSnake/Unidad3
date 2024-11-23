119544c6f897cca44c201c4d0ffc0caedfbae83b
JohnnyMSnake <l22550321@chihuahua2.tecnm.mx>
2024-11-22 19:54:59 -0600

    creacion practica 4 ejercicio 2

---
+++ js/p4e2.js
@@ -0,0 +1,35 @@
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSumbit)
+
+function handleSumbit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    if(confirm("Los datos ingresados son correctos?")){
+        let calificaciones = {
+            cal1 : "",
+            cal2 : "",
+            cal3 : "",
+            cal4 : "",
+            cal5 : "",
+            cal6 : "",
+            cal7 : "",
+            calcular : function(){
+                let promedio = (Number(this.cal1) + Number(this.cal2) + Number(this.cal3) + Number(this.cal4) + Number(this.cal5) + Number(this.cal6) + Number(this.cal7))/7
+                let resultado = document.getElementById('promedio')
+                resultado.innerHTML = "Promedio: "+promedio
+            }
+        }
+        calificaciones.cal1 = document.getElementById('c1').value
+        calificaciones.cal2 = document.getElementById('c2').value
+        calificaciones.cal3 = document.getElementById('c3').value
+        calificaciones.cal4 = document.getElementById('c4').value
+        calificaciones.cal5 = document.getElementById('c5').value
+        calificaciones.cal6 = document.getElementById('c6').value
+        calificaciones.cal7 = document.getElementById('c7').value
+        calificaciones.calcular()
+
+    }else{
+        alert("Ejecucion cancelada")
+    }
+}
